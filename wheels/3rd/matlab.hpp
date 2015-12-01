5c0360f0d89be658f1feb2720510ea2b1ee4aa7e
Hao Yang <yangh2007@gmail.com>
2015-12-01 17:50:31 +0800

    ==

---
+++ wheels/3rd/matlab.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <mex.h>
+#include <mat.h>
+
+#include "../tensor/functions.hpp"
+#include "../tensor/matrices.hpp"
+
+namespace wheels {
+
+
+
+}
---
+++ wheels/3rd/opencv.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <opencv2/opencv.hpp>
+
+#include "../tensor/functions.hpp"
+#include "../tensor/matrices.hpp"
+
+namespace wheels {
+
+
+
+}
