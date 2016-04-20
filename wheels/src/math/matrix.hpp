afd195c76ee670046c7df6d192b228f6612537b3
Yang Hao <yangh2007@gmail.com>
2016-04-20 13:59:55 +0800

    [Not Buildable]

--- wheels/src/math/matrix.hpp
+++ wheels/src/math/matrix.hpp
@@ -1,15 +1,11 @@
 #pragma once
 
 #include "../tensor/base.hpp"
+#include "../tensor/extension.hpp"
 
 #include "matrix_fwd.hpp"
 
 namespace wheels {
 
-template <class EleT, class ST, class MT, class NT, class T>
-class matrix_base : public tensor_base<EleT, tensor_shape<ST, MT, NT>,
-                                       matrix_base<EleT, ST, MT, NT, T>> {
-public:
 
-};
 }
