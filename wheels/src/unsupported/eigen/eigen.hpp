e7b03b0cbb34c56d5539ec8beb8dbfd0c77765ba
yanghao <yangh2007@gmail.com>
2016-05-01 09:18:34 +0800

    add test case for eigen conversion

--- wheels/src/unsupported/eigen/eigen.hpp
+++ wheels/src/unsupported/eigen/eigen.hpp
@@ -7,11 +7,15 @@
 
 namespace wheels {
 namespace details {
-template <class T> constexpr auto _transpose_or_not(T &&in, yes) {
-  return transpose(forward<T>(in));
+template <class RowT, class ColT, class T>
+constexpr auto _map_eigen_matrix_conditionally(RowT rows, ColT cols, T *data,
+                                               yes) {
+  return map(make_shape(rows, cols), data);
 }
-template <class T> constexpr T &&_transpose_or_not(T &&in, no) {
-  return static_cast<T &&>(in);
+template <class RowT, class ColT, class T>
+constexpr auto _map_eigen_matrix_conditionally(RowT rows, ColT cols, T *data,
+                                               no) {
+  return transpose(map(make_shape(cols, rows), data));
 }
 }
 
@@ -20,25 +24,23 @@ template <class Scalar, int Rows, int Cols, int Options, int MaxRows,
           int MaxCols>
 constexpr auto
 map(const Eigen::Matrix<Scalar, Rows, Cols, Options, MaxRows, MaxCols> &m) {
-  return details::_transpose_or_not(
-      map(make_shape(conditional(const_bool<Rows == Eigen::Dynamic>(), m.rows(),
-                                 const_int<Rows>()),
-                     conditional(const_bool<Cols == Eigen::Dynamic>(), m.cols(),
-                                 const_int<Cols>())),
-          m.data()),
-      const_bool<Options & Eigen::RowMajor>());
+  return details::_map_eigen_matrix_conditionally(
+      conditional(const_bool<Rows == Eigen::Dynamic>(), m.rows(),
+                  const_int<Rows>()),
+      conditional(const_bool<Cols == Eigen::Dynamic>(), m.cols(),
+                  const_int<Cols>()),
+      m.data(), const_bool<Options & Eigen::RowMajor>());
 }
 
 template <class Scalar, int Rows, int Cols, int Options, int MaxRows,
           int MaxCols>
 inline auto
 map(Eigen::Matrix<Scalar, Rows, Cols, Options, MaxRows, MaxCols> &m) {
-  return details::_transpose_or_not(
-      map(make_shape(conditional(const_bool<Rows == Eigen::Dynamic>(), m.rows(),
-                                 const_int<Rows>()),
-                     conditional(const_bool<Cols == Eigen::Dynamic>(), m.cols(),
-                                 const_int<Cols>())),
-          m.data()),
-      const_bool<Options & Eigen::RowMajor>());
+  return details::_map_eigen_matrix_conditionally(
+      conditional(const_bool<Rows == Eigen::Dynamic>(), m.rows(),
+                  const_int<Rows>()),
+      conditional(const_bool<Cols == Eigen::Dynamic>(), m.cols(),
+                  const_int<Cols>()),
+      m.data(), const_bool<Options & Eigen::RowMajor>());
 }
 }
--- wheels/src/unsupported/eigen/eigen.test.cpp
+++ wheels/src/unsupported/eigen/eigen.test.cpp
@@ -1,5 +1,7 @@
-#if defined(wheels_with_eigen)
 #include <gtest/gtest.h>
+
+#include "../../tensor/matrix.hpp"
+#include "../../tensor/vector.hpp"
 
 #include "eigen.hpp"
 
@@ -7,6 +9,18 @@ using namespace wheels;
 using namespace Eigen;
 
 TEST(unsupported, eigen) {
-  // TODO need tests
+  Eigen::VectorXd ev1 = Eigen::VectorXd::Random(100);
+  auto v1 = map(ev1);
+  for (int i = 0; i < v1.numel(); i++) {
+    ASSERT_EQ(v1[i], ev1[i]);
+  }
+  Eigen::MatrixXd em1 = Eigen::MatrixXd::Random(20, 30);
+  auto m1 = map(em1);
+  ASSERT_EQ(em1.rows(), m1.rows());
+  ASSERT_EQ(em1.cols(), m1.cols());
+  for (int i = 0; i < m1.rows(); i++) {
+    for (int j = 0; j < m1.cols(); j++) {
+      ASSERT_EQ(m1(i, j), em1(i, j));
+    }
+  }
 }
-#endif
